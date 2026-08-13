80c890c6b5c1f6998c6946f973dd89ee537da2a6
Drix Narciso - Mac <narcisodrix@gmail.com>
2026-08-13 18:37:39 +0800

    Initial Commit

---
+++ Orbit/OrbitApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrbitApp.swift
+//  Orbit
+//
+//  Created by Drix Narciso on 8/13/26.
+//
+
+import SwiftUI
+
+@main
+struct OrbitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OrbitTests/OrbitTests.swift
@@ -0,0 +1,18 @@
+//
+//  OrbitTests.swift
+//  OrbitTests
+//
+//  Created by Drix Narciso on 8/13/26.
+//
+
+import Testing
+
+struct OrbitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
